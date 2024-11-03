efafd749a6417534cd3a7c3aec887e30880e30e2
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-11-03 17:50:57 +0100

    (feat) compile process init

--- compiler.h
+++ compiler.h
@@ -1,7 +1,7 @@
 #ifndef PEACHCOMPILER_H
 #define PEACHCOMPILER_H
 
-#include <studio.h>
+#include <stdio.h>
 
 struct pos
 {
@@ -20,7 +20,7 @@ enum
     TOKEN_TYPE_STRING,
     TOKEN_TYPE_COMMENT,
     TOKEN_TYPE_NEWLINE,
-}
+};
 
 struct token
 {
@@ -76,3 +76,5 @@ enum
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
 };
+
+#endif 
